2ee45942ab048226eff9fd034ac9e89946271aca
Foundation O5 <Yang Er Hang>
2020-09-01 22:58:14 +0800

    add new files

---
+++ PathEdit/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace PathEdit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_add = new System.Windows.Forms.Button();
+            this.lst_value = new System.Windows.Forms.ListBox();
+            this.btn_remove = new System.Windows.Forms.Button();
+            this.btn_revert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_add
+            // 
+            this.btn_add.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_add.Location = new System.Drawing.Point(12, 414);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(53, 50);
+            this.btn_add.TabIndex = 0;
+            this.btn_add.Text = "+";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lst_value
+            // 
+            this.lst_value.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lst_value.FormattingEnabled = true;
+            this.lst_value.ItemHeight = 15;
+            this.lst_value.Location = new System.Drawing.Point(12, 12);
+            this.lst_value.Name = "lst_value";
+            this.lst_value.Size = new System.Drawing.Size(1006, 379);
+            this.lst_value.TabIndex = 1;
+            // 
+            // btn_remove
+            // 
+            this.btn_remove.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_remove.Location = new System.Drawing.Point(71, 414);
+            this.btn_remove.Name = "btn_remove";
+            this.btn_remove.Size = new System.Drawing.Size(53, 50);
+            this.btn_remove.TabIndex = 2;
+            this.btn_remove.Text = "-";
+            this.btn_remove.UseVisualStyleBackColor = true;
+            this.btn_remove.Click += new System.EventHandler(this.btn_remove_Click);
+            // 
+            // btn_revert
+            // 
+            this.btn_revert.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_revert.Location = new System.Drawing.Point(951, 414);
+            this.btn_revert.Name = "btn_revert";
+            this.btn_revert.Size = new System.Drawing.Size(67, 50);
+            this.btn_revert.TabIndex = 3;
+            this.btn_revert.Text = "Revert";
+            this.btn_revert.UseVisualStyleBackColor = true;
+            this.btn_revert.Click += new System.EventHandler(this.btnRevert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1030, 476);
+            this.Controls.Add(this.btn_revert);
+            this.Controls.Add(this.btn_remove);
+            this.Controls.Add(this.lst_value);
+            this.Controls.Add(this.btn_add);
+            this.Name = "Form1";
+            this.Text = "Path Edit v1.0";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.ListBox lst_value;
+        private System.Windows.Forms.Button btn_remove;
+        private System.Windows.Forms.Button btn_revert;
+    }
+}
+
---
+++ PathEdit/Form1.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Principal;
+using System.Security.Permissions;
+namespace PathEdit
+{ 
+    public partial class Form1 : Form
+    { 
+        public Form1()
+        {
+            InitializeComponent();
+            savedValue = new List<string>(ReadReg().Split(';'));
+            foreach (var v in savedValue)
+            {
+                lst_value.Items.Add(v);
+            }
+        }
+
+        private List<string> savedValue; 
+        
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog browserDialog = new FolderBrowserDialog();
+            if (browserDialog.ShowDialog() == DialogResult.OK)
+            {
+                lst_value.Items.Add(browserDialog.SelectedPath); 
+                WriteReg(lst_value.GetElementString()); 
+            }
+        }
+         
+        private void btn_remove_Click(object sender, EventArgs e)
+        { 
+            for (int i = lst_value.SelectedIndices.Count - 1; i >= 0; i--)// reverse-iterate
+            {
+                lst_value.Items.RemoveAt(lst_value.SelectedIndices[i]);
+                WriteReg(lst_value.GetElementString());
+            } 
+        }
+
+        private void btnRevert_Click(object sender, EventArgs e)
+        {
+            lst_value.Items.Clear();
+             
+            foreach(var v in savedValue)
+            {
+                lst_value.Items.Add(v); 
+            }
+
+            WriteReg(lst_value.GetElementString()); 
+        }
+
+        private static string ReadReg()
+        {
+            try
+            {
+                return Registry.LocalMachine.OpenSubKey(@"System\CurrentControlSet\Control\Session Manager\Environment")
+                .GetValue("Path") as string;
+            }
+            catch (Exception except)
+            {
+                MessageBox.Show(except.Message, "Error");
+                return "";
+            }
+        }
+
+        private static void WriteReg(string rawValue)
+        {
+            try
+            {
+                Registry.LocalMachine.OpenSubKey(@"System\CurrentControlSet\Control\Session Manager\Environment", true)
+              .SetValue("Path", rawValue);
+            }
+            catch (Exception except)
+            {
+                MessageBox.Show(except.Message, "Error");
+            }
+        }
+    }
+
+    public static class Extentions
+    {
+        public static string GetElementString(this ListBox listBox)
+        {
+            var stringBuilder = new StringBuilder();
+            bool first = true;
+            foreach(var item in listBox.Items)
+            {
+                if(first) 
+                    first = false; 
+                else 
+                    stringBuilder.Append(";"); 
+                stringBuilder.Append(item.ToString());
+            } 
+            return stringBuilder.ToString();
+        }
+    } 
+}
+ 
