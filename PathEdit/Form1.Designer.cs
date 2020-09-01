2a6d99144d39a82b7d125f4f73e686031ab13a68
Yang Er Hang <yeh189@126.com>
2020-09-02 01:12:25 +0800

    user/system selection tab added

--- PathEdit/Form1.Designer.cs
+++ PathEdit/Form1.Designer.cs
@@ -29,15 +29,22 @@
         private void InitializeComponent()
         {
             this.btn_add = new System.Windows.Forms.Button();
-            this.lst_value = new System.Windows.Forms.ListBox();
+            this.lst_usrVar = new System.Windows.Forms.ListBox();
             this.btn_remove = new System.Windows.Forms.Button();
             this.btn_revert = new System.Windows.Forms.Button();
+            this.tab = new System.Windows.Forms.TabControl();
+            this.tabpg_usr = new System.Windows.Forms.TabPage();
+            this.tabpg_sys = new System.Windows.Forms.TabPage();
+            this.lst_sysVar = new System.Windows.Forms.ListBox();
+            this.tab.SuspendLayout();
+            this.tabpg_usr.SuspendLayout();
+            this.tabpg_sys.SuspendLayout();
             this.SuspendLayout();
             // 
             // btn_add
             // 
             this.btn_add.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.btn_add.Location = new System.Drawing.Point(12, 414);
+            this.btn_add.Location = new System.Drawing.Point(12, 489);
             this.btn_add.Name = "btn_add";
             this.btn_add.Size = new System.Drawing.Size(53, 50);
             this.btn_add.TabIndex = 0;
@@ -45,34 +52,32 @@
             this.btn_add.UseVisualStyleBackColor = true;
             this.btn_add.Click += new System.EventHandler(this.btnAdd_Click);
             // 
-            // lst_value
+            // lst_usrVar
             // 
-            this.lst_value.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.lst_value.FormattingEnabled = true;
-            this.lst_value.ItemHeight = 15;
-            this.lst_value.Location = new System.Drawing.Point(12, 12);
-            this.lst_value.Name = "lst_value";
-            this.lst_value.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
-            this.lst_value.Size = new System.Drawing.Size(1006, 379);
-            this.lst_value.TabIndex = 1;
+            this.lst_usrVar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lst_usrVar.FormattingEnabled = true;
+            this.lst_usrVar.ItemHeight = 15;
+            this.lst_usrVar.Location = new System.Drawing.Point(3, 3);
+            this.lst_usrVar.Name = "lst_usrVar";
+            this.lst_usrVar.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.lst_usrVar.Size = new System.Drawing.Size(992, 427);
+            this.lst_usrVar.TabIndex = 1;
             // 
             // btn_remove
             // 
             this.btn_remove.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.btn_remove.Location = new System.Drawing.Point(71, 414);
+            this.btn_remove.Location = new System.Drawing.Point(71, 489);
             this.btn_remove.Name = "btn_remove";
             this.btn_remove.Size = new System.Drawing.Size(53, 50);
             this.btn_remove.TabIndex = 2;
             this.btn_remove.Text = "-";
             this.btn_remove.UseVisualStyleBackColor = true;
-            this.btn_remove.Click += new System.EventHandler(this.btn_remove_Click);
+            this.btn_remove.Click += new System.EventHandler(this.btnRemove_Click);
             // 
             // btn_revert
             // 
             this.btn_revert.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btn_revert.Location = new System.Drawing.Point(951, 414);
+            this.btn_revert.Location = new System.Drawing.Point(1019, 489);
             this.btn_revert.Name = "btn_revert";
             this.btn_revert.Size = new System.Drawing.Size(67, 50);
             this.btn_revert.TabIndex = 3;
@@ -80,26 +85,77 @@
             this.btn_revert.UseVisualStyleBackColor = true;
             this.btn_revert.Click += new System.EventHandler(this.btnRevert_Click);
             // 
+            // tab
+            // 
+            this.tab.Controls.Add(this.tabpg_usr);
+            this.tab.Controls.Add(this.tabpg_sys);
+            this.tab.Location = new System.Drawing.Point(12, 13);
+            this.tab.Name = "tab";
+            this.tab.SelectedIndex = 0;
+            this.tab.Size = new System.Drawing.Size(1006, 462);
+            this.tab.TabIndex = 4;
+            this.tab.Selected += new System.Windows.Forms.TabControlEventHandler(this.tab_Selected);
+            // 
+            // tabpg_usr
+            // 
+            this.tabpg_usr.Controls.Add(this.lst_usrVar);
+            this.tabpg_usr.Location = new System.Drawing.Point(4, 25);
+            this.tabpg_usr.Name = "tabpg_usr";
+            this.tabpg_usr.Padding = new System.Windows.Forms.Padding(3);
+            this.tabpg_usr.Size = new System.Drawing.Size(998, 433);
+            this.tabpg_usr.TabIndex = 0;
+            this.tabpg_usr.Text = "User";
+            this.tabpg_usr.UseVisualStyleBackColor = true;
+            // 
+            // tabpg_sys
+            // 
+            this.tabpg_sys.Controls.Add(this.lst_sysVar);
+            this.tabpg_sys.Location = new System.Drawing.Point(4, 25);
+            this.tabpg_sys.Name = "tabpg_sys";
+            this.tabpg_sys.Padding = new System.Windows.Forms.Padding(3);
+            this.tabpg_sys.Size = new System.Drawing.Size(998, 433);
+            this.tabpg_sys.TabIndex = 1;
+            this.tabpg_sys.Text = "System";
+            this.tabpg_sys.UseVisualStyleBackColor = true;
+            // 
+            // lst_sysVar
+            // 
+            this.lst_sysVar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lst_sysVar.FormattingEnabled = true;
+            this.lst_sysVar.ItemHeight = 15;
+            this.lst_sysVar.Location = new System.Drawing.Point(3, 3);
+            this.lst_sysVar.Name = "lst_sysVar";
+            this.lst_sysVar.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.lst_sysVar.Size = new System.Drawing.Size(992, 427);
+            this.lst_sysVar.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1030, 476);
+            this.ClientSize = new System.Drawing.Size(1098, 551);
+            this.Controls.Add(this.tab);
             this.Controls.Add(this.btn_revert);
             this.Controls.Add(this.btn_remove);
-            this.Controls.Add(this.lst_value);
             this.Controls.Add(this.btn_add);
             this.Name = "Form1";
             this.Text = "Path Edit v1.0";
+            this.tab.ResumeLayout(false);
+            this.tabpg_usr.ResumeLayout(false);
+            this.tabpg_sys.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
         private System.Windows.Forms.Button btn_add;
-        private System.Windows.Forms.ListBox lst_value;
+        private System.Windows.Forms.ListBox lst_usrVar;
         private System.Windows.Forms.Button btn_remove;
         private System.Windows.Forms.Button btn_revert;
+        private System.Windows.Forms.TabControl tab;
+        private System.Windows.Forms.TabPage tabpg_usr;
+        private System.Windows.Forms.TabPage tabpg_sys;
+        private System.Windows.Forms.ListBox lst_sysVar;
     }
 }
 
--- PathEdit/Form1.cs
+++ PathEdit/Form1.cs
@@ -18,52 +18,83 @@ namespace PathEdit
         public Form1()
         {
             InitializeComponent();
-            savedValue = new List<string>(ReadReg().Split(';'));
-            foreach (var v in savedValue)
-            {
-                lst_value.Items.Add(v);
-            }
+
+            // init savedUsrVar and savedSysVar
+            savedUsrVar = new List<string>(ReadReg(RegSource.User).Split(';'));
+            savedSysVar = new List<string>(ReadReg(RegSource.System).Split(';'));
+
+            savedUsrVar.ForEach(v => lst_usrVar.Items.Add(v));
+            savedSysVar.ForEach(v => lst_sysVar.Items.Add(v)); 
+
+           
         }
 
-        private List<string> savedValue; 
+        // 
+
+        /// <summary>
+        /// the old user PATH var
+        /// </summary>
+        private List<string> savedUsrVar;
+
+        /// <summary>
+        /// the old system PATH var 
+        /// </summary>
+        private List<string> savedSysVar;
+
+    
+        /// <summary>
+        /// are we setting user PATH or system PATH
+        /// </summary>
+        private RegSource activeSource;
+ 
+        /// <summary>
+        /// the ListBox we operate (current PATH value is saved directly in ListBox.Items), depending on activeSource
+        /// </summary>
+        private ListBox activeListBox { get => (activeSource == RegSource.System) ? lst_sysVar : lst_usrVar; }
+         
+        /// <summary>
+        /// equals savedUsrVar or savedSysVar, depending on activeSource
+        /// </summary>
+        private List<string> activeSavedVar { get => (activeSource == RegSource.System) ? savedSysVar : savedUsrVar; }
         
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            FolderBrowserDialog browserDialog = new FolderBrowserDialog();
+            var browserDialog = new FolderBrowserDialog(); 
             if (browserDialog.ShowDialog() == DialogResult.OK)
             {
-                lst_value.Items.Add(browserDialog.SelectedPath); 
-                WriteReg(lst_value.GetElementString()); 
+                activeListBox.Items.Add(browserDialog.SelectedPath); 
+                WriteReg(activeSource, activeListBox.GetElementString()); 
             }
         }
          
-        private void btn_remove_Click(object sender, EventArgs e)
+        private void btnRemove_Click(object sender, EventArgs e)
         { 
-            for (int i = lst_value.SelectedIndices.Count - 1; i >= 0; i--)// reverse-iterate
+            for (int i = activeListBox.SelectedIndices.Count - 1; i >= 0; i--)// reverse-iterate to delete
             {
-                lst_value.Items.RemoveAt(lst_value.SelectedIndices[i]);
-                WriteReg(lst_value.GetElementString());
-            } 
+                activeListBox.Items.RemoveAt(activeListBox.SelectedIndices[i]);
+            }
+            WriteReg(activeSource, activeListBox.GetElementString());
         }
 
         private void btnRevert_Click(object sender, EventArgs e)
         {
-            lst_value.Items.Clear();
-             
-            foreach(var v in savedValue)
+            activeListBox.Items.Clear(); 
+            foreach(var v in activeSavedVar)
             {
-                lst_value.Items.Add(v); 
+                activeListBox.Items.Add(v); 
             }
-
-            WriteReg(lst_value.GetElementString()); 
+            WriteReg(activeSource, activeListBox.GetElementString()); 
         }
 
-        private static string ReadReg()
+        private static string ReadReg(RegSource src)
         {
             try
             {
-                return Registry.LocalMachine.OpenSubKey(@"System\CurrentControlSet\Control\Session Manager\Environment")
-                .GetValue("Path") as string;
+                if(src == RegSource.System)
+                {
+                    return Registry.LocalMachine.OpenSubKey(@"System\CurrentControlSet\Control\Session Manager\Environment").GetValue("Path") as string;
+                } 
+                return Registry.CurrentUser.OpenSubKey(@"Environment").GetValue("Path") as string;  
             }
             catch (Exception except)
             {
@@ -72,16 +103,39 @@ namespace PathEdit
             }
         }
 
-        private static void WriteReg(string rawValue)
+        private static void WriteReg(RegSource src, string rawValue)
         {
             try
             {
-                Registry.LocalMachine.OpenSubKey(@"System\CurrentControlSet\Control\Session Manager\Environment", true)
-              .SetValue("Path", rawValue);
+                if (src == RegSource.System)
+                {
+                    Registry.LocalMachine.OpenSubKey(@"System\CurrentControlSet\Control\Session Manager\Environment", true).SetValue("Path", rawValue);
+                }
+                else
+                {
+                    Registry.CurrentUser.OpenSubKey(@"Environment", true).SetValue("Path", rawValue); 
+                } 
             }
             catch (Exception except)
             {
                 MessageBox.Show(except.Message, "Error");
+            }
+        }
+
+        enum RegSource
+        {
+            User,System
+        }
+
+        private void tab_Selected(object sender, TabControlEventArgs e)
+        {
+            if(e.TabPage == tabpg_sys)
+            {
+                activeSource = RegSource.System;
+            }
+            else
+            {
+                activeSource = RegSource.User; 
             }
         }
     }
@@ -104,4 +158,6 @@ namespace PathEdit
         }
     } 
 }
- 
+
+//%USERPROFILE%\AppData\Local\Microsoft\WindowsApps;;C:\Software\Microsoft VS Code\bin;%USERPROFILE%\.dotnet\tools
+//C:\Program Files (x86)\Intel\Intel(R) Management Engine Components\iCLS\;C:\Program Files\Intel\Intel(R) Management Engine Components\iCLS\;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\Windows\System32\OpenSSH\;C:\Program Files (x86)\Intel\Intel(R) Management Engine Components\DAL;C:\Program Files\Intel\Intel(R) Management Engine Components\DAL;C:\Program Files (x86)\Intel\Intel(R) Management Engine Components\IPT;C:\Program Files\Intel\Intel(R) Management Engine Components\IPT;C:\Program Files (x86)\NVIDIA Corporation\PhysX\Common;C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0\;C:\WINDOWS\System32\OpenSSH\;C:\Software\Git\cmd;C:\Program Files\dotnet\
